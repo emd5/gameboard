78081fb516df437959c5307ddf2ea2b2e39b7966
emd5 <emahoney5@mail.greenriver.edu>
2017-10-23 11:57:39 -0700

    "Commit files to github"

---
+++ src/main/java/GameBoard.java
@@ -0,0 +1,13 @@
+public class GameBoard {
+
+    public void addCell(Cell cell) {
+
+    }
+
+    public int getCellNumber(){
+        return 0;
+    }
+
+}
+
+
---
+++ src/test/java/GameBoardTest.java
@@ -0,0 +1,25 @@
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+
+public class GameBoardTest extends TestCase{
+
+    public GameBoardTest(String name)
+    {
+        super(name);
+    }
+
+    public void testAddCell(){
+        GameBoard gameBoard = new GameBoard ();
+        assertEquals (0, gameBoard.getCellNumber());
+
+        Cell cell = new Cell();
+        gameBoard.addCell(cell);
+        assertEquals (1,gameBoard.getCellNumber());
+    }
+
+
+
+
+}
