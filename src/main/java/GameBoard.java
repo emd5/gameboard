cf61c6ef3e2f0fc64a5b2afc5a4291c39d3e50f9
emd5 <emahoney5@mail.greenriver.edu>
2017-10-30 13:12:09 -0700

    "add global variable arraylist, constructor, implemented new methods"

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,13 +1,30 @@
+import java.util.ArrayList;
 
 public class GameBoard {
 
+    ArrayList cells = new ArrayList ();
+
+
+    public GameBoard(){
+        addCell (new GoCell ());
+    }
+
     public void addCell(Cell cell) {
 
+        cells.add(cell);
     }
 
-    public int getCellNumber(){
-        return 0;
+    public int getCellNumber() {
+
+        return cells.size ();
     }
+
+    public Cell getCell(int index){
+
+        return (Cell)cells.get (index);
+    }
+
+
 
 }
 
